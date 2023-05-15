9dc159fee1934bc07bd65b1373863643e4616b38
PDB86 <131681676+PDB86@users.noreply.github.com>
2023-05-15 14:20:03 -0400

    Reset button added DRY Code
    No local storage Yet!

--- VoteCounter/script.js
+++ VoteCounter/script.js
@@ -5,6 +5,7 @@ const containingDiv = document.getElementById("container");
 const candidateOne = document.getElementById("canOne");
 const candidateTwo = document.getElementById("canTwo");
 const candidateThree = document.getElementById("canThree");
+const resetBtn = document.getElementById("reset");
 
 // CHUPADA TEXTO DE PARAGRAPH //
 
@@ -43,13 +44,36 @@ container.addEventListener("click", (e) => {
       candidateThreeVotes++;
       totalVotes++;
     }
-    candidateOnePerc = (candidateOneVotes * 100) / totalVotes;
-    candidateTwoPerc = (candidateTwoVotes * 100) / totalVotes;
-    candidateThreePerc = (candidateThreeVotes * 100) / totalVotes;
 
-    textTotal.innerText = `the total vote count is: ${totalVotes}`;
-    textCanOne.innerText = `Candidate 1 has ${candidateOneVotes} votes and ${candidateOnePerc.toFixed(2)}% of the votes`;
-    textCanTwo.innerText = `Candidate 2 has ${candidateTwoVotes} votes and ${candidateTwoPerc.toFixed(2)}% of the votes`;
-    textCanThree.innerText = `Candidate 3 has ${candidateThreeVotes} votes and ${candidateThreePerc.toFixed(2)}% of the votes`;
+    if (e.target.innerText !== "RESET ELECTION") {
+      candidateOnePerc = (candidateOneVotes * 100) / totalVotes;
+      candidateTwoPerc = (candidateTwoVotes * 100) / totalVotes;
+      candidateThreePerc = (candidateThreeVotes * 100) / totalVotes;
+
+      textTotal.innerText = `the total vote count is: ${totalVotes}`;
+      textCanOne.innerText = `Candidate 1 has ${candidateOneVotes} votes and ${candidateOnePerc.toFixed(
+        2
+      )}% of the votes`;
+      textCanTwo.innerText = `Candidate 2 has ${candidateTwoVotes} votes and ${candidateTwoPerc.toFixed(
+        2
+      )}% of the votes`;
+      textCanThree.innerText = `Candidate 3 has ${candidateThreeVotes} votes and ${candidateThreePerc.toFixed(
+        2
+      )}% of the votes`;
+    } else if (e.target.innerText === "RESET ELECTION") {
+      totalVotes = 0;
+      candidateOneVotes = 0;
+      candidateTwoVotes = 0;
+      candidateThreeVotes = 0;
+
+      candidateOnePerc = 0;
+      candidateTwoPerc = 0;
+      candidateThreePerc = 0;
+
+      textTotal.innerText = `the total vote count is: ${totalVotes}`;
+      textCanOne.innerText = `Candidate 1 has ${candidateOneVotes} votes`;
+      textCanTwo.innerText = `Candidate 2 has ${candidateTwoVotes} votes`;
+      textCanThree.innerText = `Candidate 3 has ${candidateThreeVotes} votes`;
+    }
   }
 });
